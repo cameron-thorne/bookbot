22ba9bb7695eb648f48744e84d252f1be0ac228d
cameron-thorne <thorne.cameron@gmail.com>
2025-03-15 13:52:40 -0500

    added arguments to main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
 from stats import print_stat_report
 
-def main():
-    print_stat_report("books/frankenstein.txt")
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
+path_to_book = sys.argv[1]
 
+def main(path_to_book):
+    print_stat_report(path_to_book)
 
-main()
+main(path_to_book)
