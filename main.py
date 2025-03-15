daf31aa1df2add84b513f17e8fc0eb3627424721
cameron-thorne <thorne.cameron@gmail.com>
2025-03-14 23:08:33 -0500

    trying to get character count

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from stats import get_book_word_count
+from stats import get_char_count
 
 def main():
     get_book_word_count("books/frankenstein.txt")
+    get_char_count("books/frankenstein.txt")
 
 
 main()
--- stats.py
+++ stats.py
@@ -7,5 +7,24 @@ def get_book_word_count(path_to_file):
         print(f"{num_words} words found in the document")
 
 
-        
+def get_char_count(path_to_file):
+    with open(f"{path_to_file}") as f:
+        file_contents = f.read().split()
+        char_count = {}
+        for word in file_contents:
+            letters = word.split()
+            for letter in letters:
+                print(letter)
+                # lower_case_letter = letter.lower()
+                # if lower_case_letter in char_count:
+                #     char_count[lower_case_letter] += 1
+                # else:
+                #     char_count[lower_case_letter] = 1
+    print(char_count)
+
+
+
+
+            
+              
 
